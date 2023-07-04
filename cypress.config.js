6914c1bb792e48c882588768cedfaaa898c23399
alena-resh <reshetnikovaalenna@gmail.com>
2023-07-04 13:02:35 +0300

    learning cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,16 @@
-module.exports = {
-  'projectId': '4b7344',
-  e2e: {},
-}
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  // e2e:  {
+  //     baseUrl: "https://example.cypress.io/commands/actions",
+  //   },
+  env: {
+    navbarText: "cypress.io",
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
--- cypress/e2e/testActionspage.cy.js
+++ cypress/e2e/testActionspage.cy.js
@@ -1,30 +1,37 @@
 /// <reference types="cypress" />
 
+const navbarText = Cypress.env('navbarText');
+
 context('My first Test', () => {
     beforeEach(() => {
         cy.visit('https://example.cypress.io/commands/actions')
     })
 
-    it('Has an h1 on the page', () => {
-        cy.get('h1').should('exist');
-    })
+//     it('Has an h1 on the page', () => {
+//         cy.get('h1').should('exist');
+//     })
 
-    it('renders the correct h1 text', () => {
-        cy.get('h1').should('contain.text', 'Actions');
-    })
+//     it('renders the correct h1 text', () => {
+//         cy.get('h1').should('contain.text', 'Actions');
+//     })
 
-    it('renders a paragraph under h1', () => {
-        cy.get('.container').eq(1).find('p').should('exist');
-    })
+//     it('renders a paragraph under h1', () => {
+//         cy.get('.container').eq(1).find('p').should('exist');
+//     })
 
-    it('renders a section with correct elements', () => {
-        cy.get('.container').eq(2).within(()=> {
-        cy.get('h4').should('exist');
-        cy.get('p').should('exist');
-        })
-    })
+//     it('renders a section with correct elements', () => {
+//         cy.get('.container').eq(2).within(()=> {
+//         cy.get('h4').should('exist');
+//         cy.get('p').should('exist');
+//         })
+//     })
 
-    it('renders a paragraph under the h1', () => {
-        cy.get('.container').eq(1).find('p').should('exist');
-    })
-})
+//     it('renders a paragraph under the h1', () => {
+//         cy.get('.container').eq(1).find('p').should('exist');
+//     })
+// })
+
+       it('correctly renders the cypress website link', () => {
+       cy.findByText(navbarText).should('exist')
+       })
+  })
